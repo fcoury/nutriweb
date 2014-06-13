8e6901bd5432ccab1cdfc5b8d3f1e0b0fc09fa18
Felipe Coury <felipe.coury@gmail.com>
2014-06-12 23:35:30 -0300

    Attempt to use GET to retrieve results

--- server/src/fatsecret/api.go
+++ server/src/fatsecret/api.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/xml"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -14,6 +15,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	// "utils"
 
 	"github.com/dchest/uniuri"
 )
@@ -83,6 +86,77 @@ func ParseError(b []byte) (Error, error) {
 	return q, nil
 }
 
+func Sign(base string, secret string) (string) {
+  hasher := hmac.New(sha1.New, []byte(secret))
+  io.WriteString(hasher, base)
+
+  return base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+}
+
+func Get(getUrl string, params map[string]string) ([]byte, error) {
+	fatSecretAccessSecret := "de666f86e8634a77947c02fc39cf33cd"
+	getUrl = getUrl + "?"
+
+	for k, v := range params {
+		getUrl += k + "=" + url.QueryEscape(v) + "&"
+	}
+
+	getUrl = strings.TrimSuffix(getUrl, "&")
+
+	fmt.Println(getUrl)
+
+	sigBaseStr := "GET&" + url.QueryEscape(getUrl)
+	sharedSecret := fatSecretAccessSecret + "&"
+
+	sig := url.QueryEscape(Sign(sigBaseStr, sharedSecret))
+	fmt.Println(sigBaseStr)
+
+	getUrl += "&oauth_signature=" + sig
+
+	fmt.Println(getUrl)
+
+	resp, err := http.Get(getUrl)
+
+	fmt.Println(fmt.Sprintf("Response code: %d", resp.StatusCode))
+
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Read the content into a byte array
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func Post(postUrl string, params map[string]string) ([]byte, error) {
+	values := url.Values{}
+
+	for k, v := range params {
+		values.Add(k, v)
+	}
+
+	resp, err := http.PostForm(postUrl, values)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(fmt.Sprintf("Response code: %d", resp.StatusCode))
+
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Read the content into a byte array
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
 // This function fetch the content of a URL will return it as an
 // array of bytes if retrieved successfully.
 func Query(query string) ([]byte, error) {
@@ -90,7 +164,7 @@ func Query(query string) ([]byte, error) {
 	fatSecretConsumerKey := "62cc7c5caaf542668006fc70cbfdabae"
 	fatSecretAccessSecret := "de666f86e8634a77947c02fc39cf33cd"
 
-	oauth_nonce := uniuri.New()
+	oauth_nonce := hex.EncodeToString([]byte(uniuri.New()))
 	oauth_timestamp := strconv.FormatInt(time.Now().Unix(), 10)
 
 	reg, err := regexp.Compile("[^a-z]")
@@ -118,39 +192,12 @@ func Query(query string) ([]byte, error) {
 	paramStr = strings.TrimPrefix(paramStr, "&")
 
 	sigBaseStr := "POST&" + url.QueryEscape(fatSecretUrl) + "&" + url.QueryEscape(paramStr)
+	sharedSecret := fatSecretAccessSecret + "&"
 
-	sharedSecret := fatSecretAccessSecret + "&" // fatSecretConsumerKey + "&" + fatSecretAccessSecret
+	oauth_signature := Sign(sigBaseStr, sharedSecret)
 
-	hasher := hmac.New(sha1.New, []byte(sharedSecret))
-	io.WriteString(hasher, sigBaseStr)
-
-	oauth_signature := base64.StdEncoding.EncodeToString(hasher.Sum(nil))
 	fmt.Println("Signature: " + oauth_signature)
 
-	// Build the request
-	values := url.Values{"method": {"foods.search"},
-		"oauth_consumer_key":     {fatSecretConsumerKey},
-		"oauth_nonce":            {oauth_nonce},
-		"oauth_signature_method": {"HMAC-SHA1"},
-		"oauth_timestamp":        {oauth_timestamp},
-		"oauth_version":          {apiValues["oauth_version"]},
-		"search_expression":      {apiValues["search_expression"]},
-		"oauth_signature":        {oauth_signature}}
-
-	resp, err := http.PostForm(fatSecretUrl, values)
-	if err != nil {
-		return nil, err
-	}
-	// Defer the closing of the body
-	defer resp.Body.Close()
-
-	fmt.Println(fmt.Sprintf("Response code: %d", resp.StatusCode))
-
-	// Read the content into a byte array
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
+	body, err := Get(fatSecretUrl, apiValues)
 	return body, nil
 }
